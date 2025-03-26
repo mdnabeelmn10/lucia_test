fb9d82784771b6e5a79f206af7ca5da271f7acac
shineyjeyaraj <111896851+shineyjeyaraj@users.noreply.github.com>
2025-03-26 14:09:13 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -29,10 +29,9 @@ DEBUG = True
 import dj_database_url
 import os
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    )
 }
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
