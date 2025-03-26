690392746405af43ecd940d70325fa90efd719b9
shineyjeyaraj <111896851+shineyjeyaraj@users.noreply.github.com>
2025-03-26 12:00:43 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -28,7 +28,12 @@ DEBUG = True
 #ALLOWED_HOSTS = []
 import dj_database_url
 import os
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 ALLOWED_HOSTS = ['lucia-test.herokuapp.com']
@@ -79,12 +84,7 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
 
 
 # Password validation
