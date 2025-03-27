40d2d57d78f8f4081b55e2a106454518478bca27
shineyjeyaraj <111896851+shineyjeyaraj@users.noreply.github.com>
2025-03-27 17:26:01 +0530

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Item
+from .serializers import ItemSerializer
+@api_view(['GET'])
+def hello_world(request):
+    return Response({"message": "Hello, Shiney"})
+@api_view(['GET'])    
 def home(request):
-    return render(request, "pages/home.html", {})
+    return Response("Welcome to the Home Page!")
+
+
+@api_view(['POST'])
+def create_item(request):
+    serializer = ItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
