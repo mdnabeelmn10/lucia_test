921b0f21f9cc81ddfd2e4ceeb8ff2b75a53031cd
shineyjeyaraj <111896851+shineyjeyaraj@users.noreply.github.com>
2025-03-27 19:13:26 +0530

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -16,6 +16,6 @@ def home(request):
 def create_item(request):
     serializer = ItemSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        #serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
