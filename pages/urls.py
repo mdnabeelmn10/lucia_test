a959d7a0e438d6b7e3d2695a0e0ff2d671c060d3
shineyjeyaraj <111896851+shineyjeyaraj@users.noreply.github.com>
2025-03-27 17:23:11 +0530

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from pages import views
-
+from .views import home,create_item
 urlpatterns = [
-    path("", views.home, name="home"),
-] 
+    path('', home, name='home'),  # Root URL
+    path('items/create/', create_item, name='create_item'),
+]
