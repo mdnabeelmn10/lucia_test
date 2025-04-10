78b0b879937c6ebfc99f5c46924c82a26853bee6
shineyjeyaraj <111896851+shineyjeyaraj@users.noreply.github.com>
2025-04-10 16:04:09 +0530

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from pages import views
-from .views import home,create_item
+from .views import home,create_item,validate_login
 urlpatterns = [
     path('', home, name='home'),  # Root URL
     path('items/create/', create_item, name='create_item'),
+    path('login/validate/', validate_login, name='validate_login'),
     path('home/',home,name='home'),
 ]
