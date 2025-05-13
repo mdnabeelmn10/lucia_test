62aee5f8c917f54c9f29600b6b5c630d5166d424
daathwi <daathwi.031@gmail.com>
2025-05-13 18:15:59 +0530

    pagination added

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -5,44 +5,22 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_URLCONF = 'personal_portfolio.urls'
 
-# Secret and Debug (set properly in production)
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z$od%-!=m#3fzu2*6c3adcs9om*@$+o7o7@d&((+7rxv2jbe1y'
+
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lucia-test.herokuapp.com', 'lucia-test1-a0caeccdefd5.herokuapp.com', '127.0.0.1']
-
-# Database setup
-DATABASES = {
-    'default': dj_database_url.config(
-        # default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
-        default= 'postgres://u8v6d3k9cdqfil:pd65f4ab8483de9bc348ea378699deaea6ac9435e4e8ca04dc2a4d324856e91c4@cb5ajfjosdpmil.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d27eo25bqdg6c1'
-    )
-}
-
-# Middleware (merged)
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+ALLOWED_HOSTS = [
+    'lucia-test.herokuapp.com',
+    'lucia-test1-a0caeccdefd5.herokuapp.com',
+    '127.0.0.1',
+    'localhost'
 ]
 
-# Static files configuration
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# CORS settings
-CORS_ALLOW_ALL_ORIGINS = True  # Set this to False and specify origins in production
-
-# Installed apps (ensure CORS and WhiteNoise are included properly)
+# Application definition
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',  # Ensure this is first
+    'whitenoise.runserver_nostatic',  # should be first in the list
     'django.contrib.staticfiles',
     'jazzmin',
     'django.contrib.admin',
@@ -50,26 +28,31 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'corsheaders',
+    'corsheaders',  # CORS
     'rest_framework',
     'rest_framework_simplejwt',
-    'pages'
+    'pages',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-}
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  # 👈 must be high up
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'personal_portfolio.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',  # required for Django Admin
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # where you can add custom templates
-        'APP_DIRS': True,  # ensures templates within each app will be found
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -81,8 +64,14 @@ TEMPLATES = [
     },
 ]
 
-AUTH_USER_MODEL = 'pages.CustomUser'
+WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 
+# Database
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgres://u8v6d3k9cdqfil:pd65f4ab8483de9bc348ea378699deaea6ac9435e4e8ca04dc2a4d324856e91c4@cb5ajfjosdpmil.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d27eo25bqdg6c1'
+    )
+}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -92,10 +81,58 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Timezone and Language settings
+# Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# Static files
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom user model
+AUTH_USER_MODEL = 'pages.CustomUser'
+
+# Django REST Framework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+# CORS & CSRF configuration
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "https://luciacharitable.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://luciacharitable.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
